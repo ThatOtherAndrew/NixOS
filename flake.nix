e82566f0fc54acafec6558efd8e5526525ed655b
Andromeda <stroev.andrew@gmail.com>
2025-05-02 19:03:11 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }: {
+    nixosConfigurations = let
+      hostname = "nixos";
+    in {
+      ${hostname} = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
