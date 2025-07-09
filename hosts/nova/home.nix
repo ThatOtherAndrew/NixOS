f6b5cb1dfe3d41e3e399457766b504da7b35bb20
Andromeda <stroev.andrew@gmail.com>
2025-07-09 13:24:52 +0200

    Add basic wlogout styling

--- hosts/nova/home.nix
+++ hosts/nova/home.nix
@@ -273,7 +273,6 @@
           text = "Sleep";
           keybind = "s";
         }
-
         {
           label = "hibernate";
           action = "systemctl hibernate";
@@ -293,6 +292,68 @@
           keybind = "r";
         }
       ];
+      style = ''
+        * {
+          font-family: "JetBrainsMono Nerd Font";
+          box-shadow: none;
+        }
+
+        window {
+          background-color: rgba(0, 0, 0, 0.7)
+        }
+
+        button {
+          font-size: 23px;
+          margin: 120px 0px;
+          background-repeat: no-repeat;
+          background-position: center;
+          background-size: 20%;
+          background-color: black;
+          opacity: 0.8;
+          border: none;
+          border-radius: 0px;
+          transition: .3s cubic-bezier(0.33, 1, 0.68, 1) all;
+        }
+
+        button:focus {
+          background-size: 25%;
+          background-color: rebeccapurple;
+          border: none;
+        }
+
+        button:hover {
+          background-size: 35%;
+          background-color: rebeccapurple;
+          margin: 75px 0px;
+          border-radius: 50px;
+        }
+
+        #lock {
+          border-radius: 50px 0px 0px 50px;
+          background-image: image(url("icons/lock.png"));
+        }
+
+        #suspend {
+          background-image: image(url("icons/suspend.png"));
+        }
+
+        #hibernate {
+          background-image: image(url("icons/hibernate.png"));
+        }
+
+        #shutdown {
+          background-image: image(url("icons/shutdown.png"));
+        }
+
+        #reboot {
+          border-radius: 0px 50px 50px 0px;
+          background-image: image(url("icons/reboot.png"));
+        }
+
+        #lock:hover, #reboot:hover {
+          border-radius: 50px;
+        }
+      '';
     };
   };
 
