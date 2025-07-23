2217e37c899f460cf4e5690ff3a2fe8e3d4f9fa2
Andromeda <stroev.andrew@gmail.com>
2025-07-23 00:21:41 -0700

    Update NixOS and fix JetBrains IDEs

--- modules/jetbrains/home-manager.nix
+++ modules/jetbrains/home-manager.nix
@@ -2,7 +2,11 @@
 
 {
   home.packages = with pkgs.jetbrains; [
-    pycharm-professional
-    webstorm
+    (pycharm-professional.override {
+      jdk = pkgs.openjdk21;
+    })
+    (webstorm.override {
+      jdk = pkgs.openjdk21;
+    })
   ];
 }
