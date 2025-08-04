5138df2ca8feacb7bd1776172f67d33d9a07e17b
Andromeda <stroev.andrew@gmail.com>
2025-08-04 01:41:07 -0700

    Update flake and fix JetBrains packages

--- modules/jetbrains/home-manager.nix
+++ modules/jetbrains/home-manager.nix
@@ -2,11 +2,7 @@
 
 {
   home.packages = with pkgs.jetbrains; [
-    (pycharm-professional.override {
-      jdk = pkgs.openjdk21;
-    })
-    (webstorm.override {
-      jdk = pkgs.openjdk21;
-    })
+    pycharm-professional
+    webstorm
   ];
 }
