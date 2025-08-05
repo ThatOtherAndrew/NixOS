a28ba613032cd575569184761e226eff5544f01f
Andromeda <stroev.andrew@gmail.com>
2025-08-05 16:49:53 -0700

    Remove environment.systemPackages from host config

--- hosts/nova/default.nix
+++ hosts/nova/default.nix
@@ -117,13 +117,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-  ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
