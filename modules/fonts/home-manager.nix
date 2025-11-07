94a139ed208fe02c66153909d4ac71d25e5dfc7b
Andromeda <stroev.andrew@gmail.com>
2025-11-07 00:21:12 +0000

    Fix Twemoji

--- modules/fonts/home-manager.nix
+++ modules/fonts/home-manager.nix
@@ -4,7 +4,6 @@
   home.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     twemoji-color-font
-    twitter-color-emoji
     unifont
     unifont_upper
   ];
@@ -12,10 +11,25 @@
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      serif = [];
-      sansSerif = [];
-      monospace = ["JetBrains Mono Nerd Font"];
-      emoji = [];
+      serif = [ ];
+      sansSerif = [ ];
+      monospace = [ "JetBrains Mono Nerd Font" ];
+      emoji = [ "Twitter Color Emoji" ];
     };
   };
+
+  # xdg.configFile."fontconfig/conf.d/99-emoji-replacement.conf".text = ''
+  #   <?xml version="1.0"?>
+  #   <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+  #   <fontconfig>
+  #     <match target="pattern">
+  #       <test qual="any" name="family">
+  #         <string>Segoe UI Emoji</string>
+  #       </test>
+  #       <edit name="family" mode="assign" binding="strong">
+  #         <string>Twitter Color Emoji</string>
+  #       </edit>
+  #     </match>
+  #   </fontconfig>
+  # '';
 }
