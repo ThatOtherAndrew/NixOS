c24dd70d40f6f056fc046f16fc4fe37869b63f5a
Andromeda <stroev.andrew@gmail.com>
2025-12-28 00:37:47 +0000

    Fix python SSL

--- modules/python/default.nix
+++ modules/python/default.nix
@@ -2,4 +2,11 @@
 
 {
   home-manager.users.${vars.username} = ./home-manager.nix;
+
+  # Fix uv python ssl.SSLCertVerificationError
+  # https://discourse.nixos.org/t/fix-ssl-sslcertverificationerror-with-uvs-standalone-python
+  environment.etc.certfile = {
+    source = "/etc/ssl/certs/ca-bundle.crt";
+    target = "ssl/cert.pem";
+  };
 }
