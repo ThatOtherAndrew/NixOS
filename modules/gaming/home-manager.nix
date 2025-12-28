dd4c6cd0847b474891b9cf0cafe21a19f7015115
Andromeda <stroev.andrew@gmail.com>
2025-12-28 00:34:27 +0000

    Disable tetr.io plus

--- modules/gaming/home-manager.nix
+++ modules/gaming/home-manager.nix
@@ -3,7 +3,8 @@
 {
   home.packages = with pkgs; [
     olympus  # Celeste mod manager
-    (tetrio-desktop.override { withTetrioPlus = true; })
+    # (tetrio-desktop.override { withTetrioPlus = true; })
+    tetrio-desktop  # tetrio-plus currently broken
   ];
 
   programs.lutris.enable = true;  # Game launcher
